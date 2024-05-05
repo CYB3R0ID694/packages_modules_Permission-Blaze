GrapheneOS/platform_packages_modules_Permission

add a set of helpers for creating Storage Scopes-like UI

PermissionController/src/com/android/permissioncontroller/ext/BasePreferenceFragmentCompat.kt
@@ -1,0 +1,46 @@
+package com.android.permissioncontroller.ext
+
+import android.content.Context
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.preference.PreferenceFragmentCompat
+import com.android.permissioncontroller.permission.ui.handheld.pressBack
+
+abstract class BasePreferenceFragmentCompat : PreferenceFragmentCompat() {
+    val context_: Context
+        get() = requireContext()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true) // needed for the "back arrow" button even when there's no menu
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        preferenceScreen = preferenceManager.createPreferenceScreen(context)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        update()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        requireActivity().title = getTitle()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.pressBack()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    abstract fun update()
+
+    abstract fun getTitle(): CharSequence
+
+    val toastManager = ToastManager(this)
+}

PermissionController/src/com/android/permissioncontroller/ext/BaseSettingsWithLargeHeaderFragment.kt
@@ -1,0 +1,43 @@
+package com.android.permissioncontroller.ext
+
+import android.content.Context
+import android.os.Bundle
+import android.view.MenuItem
+import com.android.permissioncontroller.permission.ui.handheld.SettingsWithLargeHeader
+import com.android.permissioncontroller.permission.ui.handheld.pressBack
+
+abstract class BaseSettingsWithLargeHeaderFragment : SettingsWithLargeHeader() {
+    val context_: Context
+        get() = requireContext()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true) // needed for the "back arrow" button even when there's no menu
+        preferenceScreen = preferenceManager.createPreferenceScreen(context)
+    }
+
+    abstract fun getTitle(): CharSequence
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.pressBack()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        update()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        requireActivity().title = getTitle()
+    }
+
+    abstract fun update()
+
+    val toastManager = ToastManager(this)
+}

PermissionController/src/com/android/permissioncontroller/ext/ScopesUtils.kt
@@ -1,0 +1,61 @@
+package com.android.permissioncontroller.ext
+
+import android.app.AppOpsManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Process
+import com.android.permissioncontroller.permission.utils.Utils
+
+object ScopesUtils {
+
+    fun getGroupPerms(groups: Set<String>): List<String> {
+        return groups.fold(ArrayList(groups.size * 4)) { list, group ->
+            list.addAll(Utils.getPlatformPermissionNamesOfGroup(group))
+            list
+        }
+    }
+
+    // returns whether at least one permission was revoked
+    fun revokePermissions(ctx: Context, pkgName: String,
+                          perms: List<String>, appOpPerms: Array<String> = emptyArray()): Boolean {
+        val pm = ctx.packageManager
+        val uid: Int
+        val targetSdk: Int
+        try {
+            uid = pm.getPackageUid(pkgName, 0)
+            targetSdk = pm.getApplicationInfo(pkgName, 0).targetSdkVersion
+        } catch (e: PackageManager.NameNotFoundException) {
+            return false
+        }
+
+        val appOps = ctx.getSystemService(AppOpsManager::class.java)!!
+        val user = Process.myUserHandle()
+        var numOfRevokations = 0
+        for (permission in perms) {
+            if (targetSdk >= 23) { // runtime permissions are always granted for targetSdk < 23 apps
+                if (pm.checkPermission(permission, pkgName) == PackageManager.PERMISSION_GRANTED) {
+                    pm.revokeRuntimePermission(pkgName, permission, user, "Scopes")
+                    val permFlag = PackageManager.FLAG_PERMISSION_USER_SET
+                    pm.updatePermissionFlags(permission, pkgName, permFlag, permFlag, user)
+                    ++numOfRevokations
+                }
+            }
+            val op = AppOpsManager.permissionToOp(permission) ?: continue
+            if (AppOpsManager.opToDefaultMode(op) != AppOpsManager.MODE_ALLOWED
+                    // unsafeCheckOpNoThrow is a better option than noteOpNoThrow for this particular use-case
+                    && appOps.unsafeCheckOpNoThrow(op, uid, pkgName) == AppOpsManager.MODE_ALLOWED) {
+                appOps.setUidMode(op, uid, AppOpsManager.MODE_IGNORED)
+                ++numOfRevokations
+            }
+        }
+
+        for (opPerm in appOpPerms) {
+            val op = AppOpsManager.permissionToOp(opPerm)
+            if (appOps.unsafeCheckOpNoThrow(op!!, uid, pkgName) == AppOpsManager.MODE_ALLOWED) {
+                appOps.setUidMode(op, uid, AppOpsManager.MODE_ERRORED)
+                ++numOfRevokations
+            }
+        }
+        return numOfRevokations != 0
+    }
+}

CYB3R0ID694/packages_modules_Permission-Blaze

Signed-off-by: Dmitrii <bankersenator@gmail.com>

PermissionController/src/com/android/permissioncontroller/ext/BasePreferenceFragmentCompat.kt
@@ -1,0 +1,46 @@
+package com.android.permissioncontroller.ext
+
+import android.content.Context
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.preference.PreferenceFragmentCompat
+import com.android.permissioncontroller.permission.ui.handheld.pressBack
+
+abstract class BasePreferenceFragmentCompat : PreferenceFragmentCompat() {
+    val context_: Context
+        get() = requireContext()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true) // needed for the "back arrow" button even when there's no menu
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        preferenceScreen = preferenceManager.createPreferenceScreen(context_)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        update()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        requireActivity().title = getTitle()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.pressBack()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    abstract fun update()
+
+    abstract fun getTitle(): CharSequence
+
+    val toastManager = ToastManager(this)
+}

PermissionController/src/com/android/permissioncontroller/ext/BaseSettingsWithLargeHeaderFragment.kt
@@ -1,0 +1,43 @@
+package com.android.permissioncontroller.ext
+
+import android.content.Context
+import android.os.Bundle
+import android.view.MenuItem
+import com.android.permissioncontroller.permission.ui.handheld.SettingsWithLargeHeader
+import com.android.permissioncontroller.permission.ui.handheld.pressBack
+
+abstract class BaseSettingsWithLargeHeaderFragment : SettingsWithLargeHeader() {
+    val context_: Context
+        get() = requireContext()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true) // needed for the "back arrow" button even when there's no menu
+        preferenceScreen = preferenceManager.createPreferenceScreen(context_)
+    }
+
+    abstract fun getTitle(): CharSequence
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.pressBack()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        update()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        requireActivity().title = getTitle()
+    }
+
+    abstract fun update()
+
+    val toastManager = ToastManager(this)
+}

PermissionController/src/com/android/permissioncontroller/ext/ScopesUtils.kt
@@ -1,0 +1,61 @@
+package com.android.permissioncontroller.ext
+
+import android.app.AppOpsManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Process
+import com.android.permissioncontroller.permission.utils.PermissionMapping
+
+object ScopesUtils {
+
+    fun getGroupPerms(groups: Set<String>): List<String> {
+        return groups.fold(ArrayList(groups.size * 4)) { list, group ->
+            list.addAll(PermissionMapping.getPlatformPermissionNamesOfGroup(group))
+            list
+        }
+    }
+
+    // returns whether at least one permission was revoked
+    fun revokePermissions(ctx: Context, pkgName: String,
+                          perms: List<String>, appOpPerms: Array<String> = emptyArray()): Boolean {
+        val pm = ctx.packageManager
+        val uid: Int
+        val targetSdk: Int
+        try {
+            uid = pm.getPackageUid(pkgName, 0)
+            targetSdk = pm.getApplicationInfo(pkgName, 0).targetSdkVersion
+        } catch (e: PackageManager.NameNotFoundException) {
+            return false
+        }
+
+        val appOps = ctx.getSystemService(AppOpsManager::class.java)!!
+        val user = Process.myUserHandle()
+        var numOfRevokations = 0
+        for (permission in perms) {
+            if (targetSdk >= 23) { // runtime permissions are always granted for targetSdk < 23 apps
+                if (pm.checkPermission(permission, pkgName) == PackageManager.PERMISSION_GRANTED) {
+                    pm.revokeRuntimePermission(pkgName, permission, user, "Scopes")
+                    val permFlag = PackageManager.FLAG_PERMISSION_USER_SET
+                    pm.updatePermissionFlags(permission, pkgName, permFlag, permFlag, user)
+                    ++numOfRevokations
+                }
+            }
+            val op = AppOpsManager.permissionToOp(permission) ?: continue
+            if (AppOpsManager.opToDefaultMode(op) != AppOpsManager.MODE_ALLOWED
+                    // unsafeCheckOpNoThrow is a better option than noteOpNoThrow for this particular use-case
+                    && appOps.unsafeCheckOpNoThrow(op, uid, pkgName) == AppOpsManager.MODE_ALLOWED) {
+                appOps.setUidMode(op, uid, AppOpsManager.MODE_IGNORED)
+                ++numOfRevokations
+            }
+        }
+
+        for (opPerm in appOpPerms) {
+            val op = AppOpsManager.permissionToOp(opPerm)
+            if (appOps.unsafeCheckOpNoThrow(op!!, uid, pkgName) == AppOpsManager.MODE_ALLOWED) {
+                appOps.setUidMode(op, uid, AppOpsManager.MODE_ERRORED)
+                ++numOfRevokations
+            }
+        }
+        return numOfRevokations != 0
+    }
+}